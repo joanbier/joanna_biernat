dd7849e3055f79d57a7b07354d57fd42279dc85b
jbiernat <joanna.biernat@upbonus.pl>
2025-09-22 01:05:43 +0200

    removed resolver in post details route

--- src/app/features/posts/posts.routes.ts
+++ src/app/features/posts/posts.routes.ts
@@ -1,8 +1,7 @@
 import {PostsList} from './posts-list/posts-list';
 import {PostDetails} from './post-details/post-details';
-import {postResolver} from './post-details/post-resolver';
 
 export const postsRoutes = [
   { path: "", component: PostsList },
-  { path: ":postId", component: PostDetails, resolve: { post: postResolver } }
+  { path: ":postId", component: PostDetails }
 ];
