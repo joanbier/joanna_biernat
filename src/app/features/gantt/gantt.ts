c4747d2ee215ebc5004c3f57439f8196cfe1ebc6
jbiernat <joanna.biernat@upbonus.pl>
2025-09-22 01:10:04 +0200

    init gantt component

---
+++ src/app/features/gantt/gantt.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gantt',
+  imports: [],
+  templateUrl: './gantt.html',
+  styleUrl: './gantt.scss'
+})
+export class Gantt {
+
+}
