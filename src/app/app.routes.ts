85a4acf13e9892e30f871de5b9118444b1e91625
jbiernat <joanna.biernat@upbonus.pl>
2025-09-21 23:12:00 +0200

    set basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {Gantt} from './features/gantt/gantt';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "posts",
+    loadChildren: () => import("./features/posts/posts.routes").then(m => m.postsRoutes),
+    data: { title: "JsonPl - Posts" }
+  },
+  {
+    path: "gantt",
+    component: Gantt,
+    data: { title: "JsonPl - Gantt chart" }
+  },
+  {
+    path: "**",
+    redirectTo: "/posts"
+  }
+];
